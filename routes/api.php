71c713a9213dcbaa35fb83a7b5b6cace0c12ed57
JonathaEu <jonathaserpa20@gmail.com>
2023-12-09 20:30:08 -0300

    rota para teste

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\EmprestimosController;
 use App\Http\Controllers\InvestimentosController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::apiResource('/clientes', ClientesController::class);
 Route::apiResource('/emprestimos', EmprestimosController::class);
 Route::apiResource('/investimentos', InvestimentosController::class);
+Route::get('/teste', function () {
+    try {
+        $teste = DB::table('teste')->get();
+
+        return response()->json(['status' => true, 'teste' => $teste], 200);
+    } catch (Exception $e) {
+        return response()->json(['status' => false, 'Erro' => $e], 500);
+    }
+});
